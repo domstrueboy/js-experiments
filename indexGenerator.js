ee2001c63ea49005665b00e11547cbf593f9f198
domstrueboy <domstrueboy@yandex.ru>
2019-02-18 17:54:55 +0500

    index page added

---
+++ indexGenerator.js
@@ -0,0 +1,32 @@
+const path = './';
+const fs = require('fs');
+
+const projects = {};
+
+try {
+
+    fs.readdirSync(path).forEach(dir => {
+
+        if (dir.includes('.')) return;
+
+        fs.readdirSync(`${path}${dir}/`).forEach(file => {
+
+            if (file !== 'index.html') return;
+
+            const link = `/${dir}/index.html`;
+
+            projects[dir] = link;
+        });
+    });
+
+} catch (err) {
+    console.error(err);
+}
+
+try {
+
+    fs.writeFileSync(`${path}/projects.json`, JSON.stringify(projects));
+
+} catch (err) {
+    console.error(err);
+}
