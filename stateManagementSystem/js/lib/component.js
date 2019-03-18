807780c77f724af6b98da6a8383f65934b589672
p.bobylev <p.bobylev@info74.ru>
2019-03-18 18:36:10 +0500

    component.js finished

--- stateManagementSystem/js/lib/component.js
+++ stateManagementSystem/js/lib/component.js
@@ -1,5 +1,17 @@
 import Store from '../store/store';
 
 export default class Component {
-  
+  constructor(props = {}) {
+    const self = this;
+
+    this.render = this.render || function () {};
+
+    if (props.store instanceof Store) {
+      props.store.events.subscribe('stateChange', () => self.render());
+    }
+
+    if (Object.prototype.hasOwnProperty.call(props, 'element')) {
+      this.element = props.element;
+    }
+  }
 }
