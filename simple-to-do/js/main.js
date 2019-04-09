ecfaa0b893e24fc7dd5a6f567f593f27389fd615
p.bobylev <p.bobylev@info74.ru>
2019-04-09 17:48:39 +0500

    failed atempt to do a new version with old code

--- simple-to-do/js/main.js
+++ simple-to-do/js/main.js
@@ -1,13 +1,5 @@
-const input = document.querySelector('.to-do-input');
-const list = document.querySelector('.to-do-list');
-const todos = initTodos();
-
-list.innerHTML = compileList();
-input.addEventListener('keypress', keypressHandler);
-list.addEventListener('click', clickHandler);
-
-function initTodos() {
-  const store = localStorage.getItem('todos');
+function init(key) {
+  const store = localStorage.getItem(key);
   if (store) {
     return store.split(',');
   } else {
@@ -23,33 +15,59 @@ function keypressHandler(event) {
   }
 }
 
+function doneItem() {
+  const items = [...document.querySelectorAll('.to-do-item')];
+  const indexOfDoneItem = items.indexOf(event.target.parentNode);
+  if (done.includes(items[indexOfDoneItem])) return;
+  done.push(items[indexOfDoneItem]);
+  console.log(items[indexOfDoneItem]);
+  list.innerHTML = compileList();
+}
+
+function deleteItem() {
+  const items = [...document.querySelectorAll('.to-do-item')];
+  const indexOfDeletedItem = items.indexOf(event.target.parentNode);
+  todos.splice(indexOfDeletedItem, 1);
+  list.innerHTML = compileList();
+}
+
 function clickHandler(event) {
+  if (event.target.classList.contains('to-do-done')) {
+    doneItem();
+  }
   if (event.target.classList.contains('to-do-delete')) {
-    const items = [...document.querySelectorAll('.to-do-item')];
-    const indexOfDeletedItem = items.indexOf(event.target.parentNode);
-    todos.splice(indexOfDeletedItem, 1);
-    list.innerHTML = compileList();
+    deleteItem();
   }
 }
 
 function compileList() {
   localStorage.setItem('todos', todos);
+  localStorage.setItem('done', done);
   if (todos.length > 0) {
     let listHtml = '';
     for (let todo of todos) {
       listHtml = listHtml + todoTemplate(todo);
     }
     return listHtml;
-  } else {
-    return '<li class="to-do-item">Nothing to show</li>';
   }
+  return '<li class="to-do-item">Nothing to show</li>';
 }
 
 function todoTemplate(text) {
   return `
         <li class="to-do-item">
-          ${text}
+          <span class="to-do-text">${text}</span>
+          <button class="to-do-done">✔️</button>
           <button class="to-do-delete">❌</button>
         </li>
       `;
 }
+
+const input = document.querySelector('.to-do-input');
+const list = document.querySelector('.to-do-list');
+const todos = init('todos');
+const done = init('done');
+
+list.innerHTML = compileList();
+input.addEventListener('keypress', keypressHandler);
+list.addEventListener('click', clickHandler);
