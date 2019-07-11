6924d086999f6e4a53ec67705154c5127c468ef8
Pavel Bobylyov <domstrueboy@yandex.ru>
2019-07-12 01:53:41 +0500

    image changes

--- bmpRotation/js/convert.js
+++ bmpRotation/js/convert.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-plusplus */
 export default function convert(buffer) {
+  console.log(buffer);
+  const int8ArrayView = new Int8Array(buffer);
+  for (let i = 69, len = int8ArrayView.length; i < len; i += 3) {
+    int8ArrayView[i] += 100;
+  }
   return buffer;
 }
