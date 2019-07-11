2831cfaafecf18ba3ef7876b15d6675757de1c35
Pavel Bobylyov <domstrueboy@yandex.ru>
2019-07-12 00:43:06 +0500

    everything works but actually doesn't work (convertion isn't ready yet)

--- bmpRotation/js/main.js
+++ bmpRotation/js/main.js
@@ -1,7 +1,42 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-param-reassign */
+import convert from './convert.js';
+
 class Main {
   constructor() {
+    this.selectorsAssign();
+    this.listenersAssign();
+  }
+
+  selectorsAssign() {
     this.input = document.querySelector('input');
-    console.log(this.input);
+    this.inImage = document.querySelector('.inImage');
+    this.outImage = document.querySelector('.outImage');
+  }
+
+  listenersAssign() {
+    this.input.addEventListener('change', () => {
+      this.processImage();
+    });
+  }
+
+  processImage() {
+    [this.inImageFile] = this.input.files;
+    this.displayImage(this.inImage, this.inImageFile);
+
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(this.inImageFile);
+    reader.onload = (e) => {
+      this.outImageFile = new File([convert(e.target.result)], 'outImageFile.bmp');
+      this.displayImage(this.outImage, this.outImageFile);
+    };
+  }
+
+  displayImage(image, fileToDisplay) {
+    image.src = window.URL.createObjectURL(fileToDisplay);
+    image.onload = () => {
+      window.URL.revokeObjectURL(this.src);
+    };
   }
 }
 
