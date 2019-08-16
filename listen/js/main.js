9dd66541d37f09d8caae8a8efb73715e8f2ecbf8
Pavel Bobylyov <domstrueboy@yandex.ru>
2019-08-16 23:58:05 +0500

    Some lazy changes

--- listen/js/main.js
+++ listen/js/main.js
@@ -10,8 +10,7 @@ class Main {
 
   selectorsAssign() {
     this.input = document.querySelector('input');
-    this.inImage = document.querySelector('.in-image');
-    this.outImage = document.querySelector('.out-image');
+    this.inImage = document.querySelector('.track');
     this.downloadLink = document.querySelector('.download-link');
   }
 
@@ -28,8 +27,9 @@ class Main {
     const reader = new FileReader();
     reader.readAsArrayBuffer(this.inImageFile);
     reader.onload = (e) => {
-      this.outImageFile = new File([convert(e.target.result)], '', { type: 'image/bmp' });
-      this.displayImage(this.outImage, this.outImageFile);
+      // this.outImageFile = new File([convert(e.target.result)], '', { type: 'audio/mpeg' });
+      this.outImageFile = new File([e.target.result], '', { type: 'audio/mpeg' });
+      // this.displayImage(this.outImage, this.outImageFile);
       this.showFileLink(this.outImageFile);
     };
   }
