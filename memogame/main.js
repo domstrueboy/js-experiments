c41d5c9423d55c8a1d62a299c5b9f17bb76c816c
Pavel Bobylyov <domstrueboy@yandex.ru>
2019-08-27 00:47:34 +0500

    game works

--- memogame/main.js
+++ memogame/main.js
@@ -1,37 +1,44 @@
 const set = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 const doubledSet = [...set, ...set];
 const shuffledSet = shuffle(doubledSet);
-const shuffledCards = shuffledSet.map((letter, index) => {
-    return {
-        id: index,
-        value: letter,
-        opened: false,
-        finished: false
-    }
+const cards = shuffledSet.map(letter => ({
+  value: letter,
+  opened: false,
+  finished: false,
+}));
+
+const app = new window.Vue({
+  el: '#app',
+  data: {
+    cards,
+    previousCard: null,
+  },
+  methods: {
+    handleClick(card) {
+      cards.forEach((el) => { el.opened = false; });
+      card.opened = true;
+      if (card && this.previousCard && card.value === this.previousCard.value) {
+        card.finished = true;
+        this.previousCard.finished = true;
+      }
+      this.previousCard = card;
+    },
+  },
 });
 
-const app = new Vue({
-    el: '#app',
-    data: {
-        shuffledCards: shuffledCards
-    },
-    computed: {
-        cards: () => shuffledCards.filter(card => !card.finished)
-    }
-})
+function shuffle(incomingArray) {
+  const array = incomingArray.slice();
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
 
-function shuffle (incomingArray) {
-    const array = incomingArray.slice();
-	let currentIndex = array.length;
-	let temporaryValue, randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-
-	return array;
-};
+  return array;
+}
